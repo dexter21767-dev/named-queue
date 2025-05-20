f37c3376d5d91320b2acdf1e559b995fe58b8c37
dexter21767-dev <dexter21767@gmail.com>
2025-05-20 19:38:32 +0100

    add stats

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # named-queue
-like async.queue, but tasks are named and de-duplicated
+like async.queue, but tasks are named and de-duplicated, with built-in statistics tracking
 
 ## Init
 
@@ -20,3 +20,43 @@ like async.queue, but tasks are named and de-duplicated
 ### ``queue.unshift(task, cb)``
 
 ### ``queue.length()``
+
+## Statistics Tracking
+
+The queue now includes built-in statistics tracking to help monitor performance.
+
+### ``queue.getStats()``
+
+Returns an object with the following statistics:
+
+- `queueLength`: Current number of tasks waiting in the queue
+- `activeTaskCount`: Number of tasks currently being processed
+- `maxConcurrent`: Maximum number of tasks that have ever been processed concurrently
+- `totalProcessed`: Total number of tasks processed since queue creation or last reset
+- `duplicateTaskCalls`: Number of duplicate task calls (tasks with same ID already in progress)
+- `averageWaitTime`: Average time tasks spend waiting in the queue (in milliseconds)
+- `taskStats`: Array of per-task statistics, including:
+  - `id`: Task ID
+  - `count`: Number of times this task has been processed
+  - `averageTime`: Average processing time for this task (in milliseconds)
+
+### ``queue.resetStats()``
+
+Resets all statistics tracking to zero. Returns the queue object for chaining.
+
+## Example
+
+```javascript
+const queue = new namedQueue(processTask, 3);
+
+// Add tasks to the queue
+queue.push({id: 'task1', data: 'example'}, callback);
+
+// Get current queue statistics
+const stats = queue.getStats();
+console.log(`Tasks processed: ${stats.totalProcessed}`);
+console.log(`Average wait time: ${stats.averageWaitTime}ms`);
+
+// Reset statistics if needed
+queue.resetStats();
+```
--- index.js
+++ index.js
@@ -3,58 +3,130 @@ function namedQueue(processor, concurrency) {
 
 	var waiting = []
 
-	var inProg = { }
+	var inProg = {}
 	var count = 0
 
 	//var paused = false
 
+	// Statistics tracking
+	var stats = {
+		totalProcessed: 0,
+		currentlyProcessing: 0,
+		maxConcurrent: 0,
+		waitTimes: [],
+		processingTimes: {},
+		taskHistory: {},
+		startTimes: {},
+		duplicateTaskCalls: 0
+	}
+
 	function update() {
-		while (waiting.length && count < concurrency) (function() {
+		while (waiting.length && count < concurrency) (function () {
 			var t = waiting.shift()
-			
+
 			if (inProg[t.task.id]) {
 				inProg[t.task.id].push(t.cb)
+				stats.duplicateTaskCalls++
 				return
 			} else {
+				// Record task start time
+				stats.startTimes[t.task.id] = Date.now()
+				if (!stats.taskHistory[t.task.id]) {
+					stats.taskHistory[t.task.id] = { count: 0, totalTime: 0 }
+				}
+
 				inProg[t.task.id] = [t.cb]
 				count++
-				processor(t.task, function() {
+				stats.currentlyProcessing = count
+				stats.maxConcurrent = Math.max(stats.maxConcurrent, count)
+
+				// Record wait time
+				if (t.queuedAt) {
+					var waitTime = Date.now() - t.queuedAt
+					stats.waitTimes.push(waitTime)
+				}
+
+				processor(t.task, function () {
 					var args = arguments
 
 					if (!inProg[t.task.id]) return // probably callback called twice
-	
+
+					// Record processing time
+					var processingTime = Date.now() - stats.startTimes[t.task.id]
+					stats.processingTimes[t.task.id] = processingTime
+					stats.taskHistory[t.task.id].count++
+					stats.taskHistory[t.task.id].totalTime += processingTime
+
 					count--
-					inProg[t.task.id].forEach(function(cb) { cb.apply(null, args) })
+					stats.totalProcessed++
+					stats.currentlyProcessing = count
+
+					inProg[t.task.id].forEach(function (cb) { cb.apply(null, args) })
 					delete inProg[t.task.id]
-	
+					delete stats.startTimes[t.task.id]
+
 					setImmediate(update)
 				})
-			}	
+			}
 		})()
 	}
 
-	this.push = function(task, cb) {
+	this.push = function (task, cb) {
 		if (!task.hasOwnProperty('id')) throw new Error('no task.id')
-
-		if (inProg[task.id]) { 
+		if (inProg[task.id]) {
 			inProg[task.id].push(cb)
-			return 
+			stats.duplicateTaskCalls++
+			return
 		}
-		waiting.push({ task: task, cb: cb })
+		waiting.push({ task: task, cb: cb, queuedAt: Date.now() })
 		setImmediate(update)
 	}
 
-	this.unshift = function(task, cb) {
-		if (inProg[task.id]) { 
+	this.unshift = function (task, cb) {
+		if (inProg[task.id]) {
 			inProg[task.id].push(cb)
-			return 
+			stats.duplicateTaskCalls++
+			return
 		}
-		waiting.unshift({ task: task, cb: cb })
+		waiting.unshift({ task: task, cb: cb, queuedAt: Date.now() })
 		setImmediate(update)
 	}
 
-	this.length = function() {
+	this.length = function () {
 		return waiting.length
+	}
+
+	// Statistics methods
+	this.getStats = function () {
+		return {
+			queueLength: waiting.length,
+			activeTaskCount: count,
+			maxConcurrent: stats.maxConcurrent,
+			totalProcessed: stats.totalProcessed,
+			duplicateTaskCalls: stats.duplicateTaskCalls,
+			averageWaitTime: stats.waitTimes.length > 0 ?
+				stats.waitTimes.reduce((a, b) => a + b, 0) / stats.waitTimes.length : 0,
+			taskStats: Object.keys(stats.taskHistory).map(id => ({
+				id: id,
+				count: stats.taskHistory[id].count,
+				averageTime: stats.taskHistory[id].count > 0 ?
+					stats.taskHistory[id].totalTime / stats.taskHistory[id].count : 0
+			}))
+		}
+	}
+
+	this.resetStats = function () {
+		stats = {
+			totalProcessed: 0,
+			currentlyProcessing: 0,
+			maxConcurrent: 0,
+			waitTimes: [],
+			processingTimes: {},
+			taskHistory: {},
+			startTimes: {},
+			duplicateTaskCalls: 0
+		}
+		return this
 	}
 }
 
